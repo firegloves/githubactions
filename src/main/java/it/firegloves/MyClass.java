29019a01903f1357d7edb0b71c894b03f5b75af0
firegloves <lucorset@gmail.com>
2021-01-18 11:56:40 +0100

    update on master

--- src/main/java/it/firegloves/MyClass.java
+++ src/main/java/it/firegloves/MyClass.java
@@ -10,4 +10,8 @@ public class MyClass {
     public String sayKobraKai() {
         return "Kobra Kai!";
     }
+    
+    public String sayKobraKaiAhiAhi() {
+        return "Kobra Kai!";
+    }
 }
